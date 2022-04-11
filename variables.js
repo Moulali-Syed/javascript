fd028030f255a2058bcac455fe4c48d1b10dcf41
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-11 14:45:59 +0530

    varaibles-comments

---
+++ variables.js
@@ -0,0 +1,27 @@
+//var
+var age1 = 3;
+var name1 = 'apple1';
+var dev1 = true;
+var age1 = 4;
+//declaring variable again with same name allowed with var
+
+//let
+let age = 2;
+let name = 'apple';
+let dev = true;
+let location = null;
+let task;
+// let age = 50;
+//decalaring varibale with same name is not allowed with let
+
+//const
+const age2 = 1;
+// age2 = 3;
+//reassigning to a constant varaible is not allowed
+console.log(age2);
+
+//comments
+//single line - //
+/*
+    multi line comments
+*/
