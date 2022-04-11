f036efb52dd382e7a5a1ffe25354e26dabe25d16
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-11 15:51:58 +0530

    ifelse switch ternary

---
+++ ifelse.js
@@ -0,0 +1,23 @@
+if (10 > 20) {
+  console.log('hello');
+} else {
+  console.log('hey');
+}
+//terenary operator
+
+let x = 5 > 0 ? 'yes' : 'no';
+console.log(x); //yes
+
+///Switch
+
+let a = 'monday';
+switch (a) {
+  case 'monday':
+    console.log('Monday');
+    break;
+  case 'tuesday':
+    console.log('Tuesday');
+    break;
+  default:
+    console.log('Friday');
+}
