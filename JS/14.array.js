b89ecc65fa32919f0accf65f19ea14bb226c1c08
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-25 12:01:54 +0530

    array

---
+++ JS/14.array.js
@@ -0,0 +1,7 @@
+let arr = ['apple', 'banana', 'grapes', 'mango'];
+console.log(arr);
+
+let book = ['apple', 'banana', 'grapes', 'mango'];
+let book1 = new Array('apple', 'banana', 'grapes', 'mango');
+
+console.log(book[0]);
