7a352cbfa1b564c8c993c1c90db06c8cded2ca3d
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-25 11:06:06 +0530

    datatypes

---
+++ JS/05-dataTypes.js
@@ -0,0 +1,34 @@
+let x = 5;
+x = 'hello';
+//datatype can be changed in js
+
+let p = 'happy';
+let q = 'hey';
+let r = "hey Ther'e";
+
+//escape sequence - \
+let a = 'happ"y';
+
+let s = -34.5;
+//boolean : true or false
+let c = 2 > 3;
+console.log(c); //false
+
+//array
+let x1 = ['apple', 'banana', 'mango'];
+
+//object
+let p1 = { name: 'apple', age: 1 };
+let h1;
+console.log(h1); //undefined
+
+let v = '';
+console.log(v);
+
+//to know type of variable
+// typeof
+
+console.log(typeof v);
+
+//template literals
+console.log(`hello ${x1}`);
