efa25472d494a07db9afa92e3f9d33e67ca02af4
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-25 11:11:57 +0530

    comparison-logical

---
+++ JS/06-comparison-logical.js
@@ -0,0 +1,33 @@
+// == (checks only value)
+console.log(5 == 5);
+console.log(5 == 6);
+console.log(5 == '5');
+
+// === (checks datatype)
+console.log(5 === 5);
+console.log(5 === 6);
+console.log(5 === '5');
+
+// != not equal
+// jj !== not equal with type comparison
+
+// > greater than
+// < less than
+
+// >= , <=
+
+/*
+
+ && - logical anf
+(5<10 && 6>1)--> true
+*/
+
+/*
+
+
+|| - logical or
+
+
+!   - logical not
+
+*/
