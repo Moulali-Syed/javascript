53fda600c293dd687928f3a29356eafe8f0b6794
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-26 15:32:05 +0530

    datatypes at a glance

---
+++ JS/64.dataTypesatGlance.js
@@ -0,0 +1,19 @@
+console.log(typeof 'apple');
+console.log(typeof 1);
+console.log(typeof true);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof [1, 2, 3]); //object
+console.log(typeof { name: 'apple' }); //object
+console.log(typeof NaN); //number
+/*
+string
+number
+boolean
+object
+undefined
+object
+object
+number
+*/
+// we have symbol datatype
