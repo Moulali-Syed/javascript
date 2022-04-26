d7ad59dd62a1ade397b4b4f66973eb28a3dd4beb
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-26 14:56:00 +0530

    map function

---
+++ JS/60.map.js
@@ -0,0 +1,14 @@
+//map function
+
+let arr = [2, 3, 4, 5, 6];
+
+let double = arr.map((num) => num * 2);
+console.log(double, arr);
+//[ 4, 6, 8, 10, 12 ] [ 2, 3, 4, 5, 6 ]
+
+/*
+let arr1 = [];
+for (let i = 0; i < arr.length; i++) {
+  arr1[i] = arr[i] * 2;
+}
+*/
