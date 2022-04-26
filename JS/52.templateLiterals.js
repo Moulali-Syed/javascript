4c0ec97d1f7c362f384235efa6139bf12b4ce505
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-26 11:58:41 +0530

    template literal

---
+++ JS/52.templateLiterals.js
@@ -0,0 +1,17 @@
+let str = 'hello \n world';
+console.log(str);
+
+let str1 = `hello 
+
+world`;
+console.log(str1);
+/*
+hello 
+ world
+hello 
+
+world
+*/
+
+let str3 = 'apple';
+console.log(`my name is ${str3}. ${2 * 2}`);
