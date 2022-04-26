721345a33d9f072385906e3def715b333ea81c06
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-26 11:35:25 +0530

    navigator object

---
+++ JS/40.navigatorObject.js
@@ -0,0 +1,8 @@
+//we can get browser capabilities
+console.log(navigator.appName);
+console.log(navigator.appVersion);
+console.log(navigator.appCodeName);
+console.log(navigator.cookieEnabled);
+console.log(navigator.userAgent);
+console.log(navigator.platform);
+console.log(navigator.javaEnabled());
