aae98566c5a5a487e9723251553087ac5db0a707
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-26 11:44:27 +0530

    let and const

---
+++ JS/47.let-const.js
@@ -0,0 +1,21 @@
+var cou = 1;
+let count = 1;
+const c1 = 1;
+
+//diff between var and let
+/*
+when we decalre variable with var it becomes global object property , that it is accessed with window
+but let will not become
+
+var c =1;
+var c;
+this will not throw error
+
+let c1=1;
+let c1;
+this is error with let
+
+const count2 =1;
+cant chnage
+
+*/
