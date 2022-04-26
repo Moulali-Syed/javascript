7c9a3fb5313b0293d93750f10903df52a4d245c6
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-26 11:32:35 +0530

    location object

---
+++ JS/39.locationObject.js
@@ -0,0 +1,11 @@
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.protocol);
+
+//we can redirect using location.href
+
+console.log(location.assign); // will have back button
+
+location.replace('https://google.com'); //will not in browser history, no back
+
+location.reload();
